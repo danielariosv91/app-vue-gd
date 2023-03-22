e653d18f02c4502f358067d51d0ba731c56510cd
Daniela Rios <daniela.rios.v91@gmail.com>
2023-03-22 12:24:37 -0300

    filtered by comuna

--- pages/index.vue
+++ pages/index.vue
@@ -1,21 +1,51 @@
 <template>
   <v-row justify="center" align="center">
     <v-col cols="12" sm="8" md="12">
+
+      <v-select label="Comunas" @change="onChange($event)" :items="Comunas"></v-select>
+      <v-select label="Codigos" @change="onChange($event)" :items="Codigos"></v-select>
+
       <v-card>
         <v-card-title color="">
           {{ title }}
           <v-spacer></v-spacer>
           <v-text-field v-model="search" append-icon="mdi-magnify" label="Buscar" single-line hide-details></v-text-field>
         </v-card-title>
-        <v-data-table :headers="headers" :items="Records" :search="search" item-value="name" class="elevation-1">
+        <v-data-table :headers="headers" :items="Records" :search="search" @click:row="handleClick"
+          :footer-props="footerProps" item-value="name" class="elevation-1">
           <template v-slot:column.name="{ column }">
             {{ column.title.toUpperCase() }}
           </template>
         </v-data-table>
       </v-card>
+
+
+      <div class="text-center">
+        <v-dialog v-model="dialog" width="auto">
+          <v-card>
+            <v-card-text>
+              <div class="dialog __content">
+                <h3> Horario Estación {{ station }}</h3>
+                <p>{{ schedule }}</p>
+              </div>
+            </v-card-text>
+            <v-card-actions>
+              <v-btn color="primary" block @click="dialog = false">Cerrar</v-btn>
+            </v-card-actions>
+          </v-card>
+        </v-dialog>
+      </div>
     </v-col>
   </v-row>
 </template>
+
+<style>
+.dialog.__content {
+  text-align: center;
+  line-height: 1.5;
+  padding: 10%;
+}
+</style>
 
 <script>
 export default {
@@ -23,6 +53,9 @@ export default {
     return {
       title: 'METRO',
       search: '',
+      dialog: false,
+      schedule: '',
+      station: '',
       headers: [
         {
           text: 'Nombre Fantasia',
@@ -47,16 +80,33 @@ export default {
           align: 'start',
           sortable: true,
           value: 'CODIGO',
-        }
+        },
       ],
+      footerProps: { 'items-per-page-options': [10, 20, 50] },
     }
   },
   async fetch({ store }) {
-    await store.dispatch('getRecords')
+    await store.dispatch('getRecords', '')
   },
   computed: {
     Records() {
       return this.$store.state.records
+    },
+    Comunas() {
+      return this.$store.state.comunas
+    },
+    Codigos() {
+      return this.$store.state.codigos
+    }
+  },
+  methods: {
+    handleClick(row) {
+      this.dialog = true;
+      this.station = row['NOMBRE FANTASIA'];
+      this.schedule = row['HORARIO REFERENCIAL'];
+    },
+    onChange(value) {
+      this.$store.dispatch('getRecordsFiltered', value);
     }
   }
 }
--- store/index.js
+++ store/index.js
@@ -3,12 +3,20 @@ import Vuex from 'vuex';
 const createStore = () => {
     return new Vuex.Store({
         state: {
-            records: []
+            records: [],
+            comunas: [],
+            codigos: []
         },
         mutations: {
             setRecords: (state, records) => {
                 state.records = records
-            }
+            },
+            setComunas: (state, comunas) => {
+                state.comunas = comunas
+            },
+            setCodigos: (state, codigos) => {
+                state.codigos = codigos
+            },
         },
         actions: {
             async getRecords({ commit }) {
@@ -19,7 +27,29 @@ const createStore = () => {
                     "offset": 0
                 }
                 let response = await this.$axios.$post('https://datos.gob.cl/api/3/action/datastore_search', bodyRequest);
-                commit('setRecords', response.result.records)
+                commit('setRecords', response.result.records);
+
+                let comunas = response.result.records.map(item => (item.COMUNA));
+                let uniqueComunas = [...new Set(comunas)];
+                commit('setComunas', uniqueComunas);
+
+                let codigos = response.result.records.map(item => (item.CODIGO));
+                let uniqueCodigos = [...new Set(codigos)];
+                commit('setCodigos', uniqueCodigos);
+            },
+            async getRecordsFiltered({ commit }, filter) {
+                let bodyRequest = {
+                    "resource_id": "3d54e961-d81b-4507-aeee-7a433e00a9bf",
+                    "filters": {
+                        "COMUNA": [
+                            filter
+                        ]
+                    },
+                    "limit": 25,
+                    "offset": 0
+                }
+                let response = await this.$axios.$post('https://datos.gob.cl/api/3/action/datastore_search', bodyRequest);
+                commit('setRecords', response.result.records);
             },
         }
     })
